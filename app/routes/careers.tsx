aee5bf1c86ff7ef34bb6d2344e1f22325d942a58
ahmedsherif <ahmedsherifoo23@gmail.com>
2025-07-20 23:57:35 +0300

    fixing a bug

--- app/routes/careers.tsx
+++ app/routes/careers.tsx
@@ -7,7 +7,7 @@ export default function Careers() {
     <main>
       <Navbar />
       <h1 className="text-4xl font-bold mt-8">Our Careers</h1>
-      <p className="mt-4">This is the Careers page.</p>
+      <p className="mt-4">This is the agency Careers page .</p>
     </main>
   );
 }
